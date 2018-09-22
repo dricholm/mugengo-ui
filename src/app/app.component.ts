c686ab5cf4de9c1c2c7491992daf5a57c6979855
David Richolm <david.richolm@gmail.com>
2018-09-22 13:40:12 +0200

    [feat] Added packages and basic setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'mgg-root',
+  styleUrls: ['./app.component.scss'],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'mgg';
-}
+export class AppComponent {}
