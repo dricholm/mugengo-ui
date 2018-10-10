3eaf4ff40d08b05c054f25da35ab6499ca1ab1cd
David Richolm <david.richolm@gmail.com>
2018-10-10 11:03:03 +0200

    [feat] Added icon to NavComponent

--- src/app/shared/font-awesome.ts
+++ src/app/shared/font-awesome.ts
@@ -1,7 +1,12 @@
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { faCircleNotch, faUserPlus } from '@fortawesome/free-solid-svg-icons';
+import {
+  faCircleNotch,
+  faHome,
+  faUserPlus,
+} from '@fortawesome/free-solid-svg-icons';
 
 export const initFontAwesomeLibrary = () => {
   library.add(faCircleNotch);
+  library.add(faHome);
   library.add(faUserPlus);
 };
