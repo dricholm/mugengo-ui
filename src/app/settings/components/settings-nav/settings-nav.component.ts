00060cb55a58208b3993df331cc6664df9284400
David Richolm <david.richolm@gmail.com>
2018-10-15 11:44:38 +0200

    [ref] Made Profile settings form responsive

--- src/app/settings/components/settings-nav/settings-nav.component.ts
+++ src/app/settings/components/settings-nav/settings-nav.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
+  changeDetection: ChangeDetectionStrategy.OnPush,
   selector: 'mgg-settings-nav',
   templateUrl: './settings-nav.component.html',
 })
