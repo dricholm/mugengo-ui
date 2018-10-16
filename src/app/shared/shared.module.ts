a98c7289b57d2252310a859555fba216ab255578
David Richolm <david.richolm@gmail.com>
2018-10-16 13:01:02 +0200

    [ref] Created LanguageSelectorComponent

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,22 +1,31 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { TooltipModule } from 'ngx-bootstrap/tooltip';
+import { TypeaheadModule } from 'ngx-bootstrap/typeahead';
 
 import { initFontAwesomeLibrary } from './font-awesome';
 import { LanguageIconsComponent } from './components/language-icons/language-icons.component';
+import { LanguageSelectorComponent } from './components/language-selector/language-selector.component';
 
 initFontAwesomeLibrary();
 
 @NgModule({
-  declarations: [LanguageIconsComponent],
-  exports: [ReactiveFormsModule, FontAwesomeModule, LanguageIconsComponent],
+  declarations: [LanguageIconsComponent, LanguageSelectorComponent],
+  exports: [
+    ReactiveFormsModule,
+    FontAwesomeModule,
+    LanguageIconsComponent,
+    LanguageSelectorComponent,
+  ],
   imports: [
     CommonModule,
+    FormsModule,
     ReactiveFormsModule,
     FontAwesomeModule,
     TooltipModule,
+    TypeaheadModule,
   ],
 })
 export class SharedModule {}
